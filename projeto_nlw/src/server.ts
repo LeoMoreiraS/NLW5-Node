66846623769401dcc23cc101ba9686045e538aef
Leonardo Moreira <leomoreira.com@gmail.com>
2021-04-24 17:56:28 -0300

    chat finalizado + estilizações

--- projeto_nlw/src/server.ts
+++ projeto_nlw/src/server.ts
@@ -1,7 +1,8 @@
 import { http } from "./http";
-import "./websockets/client"
+import "./websockets/client";
+import "./websockets/admin";
 
-http.listen(3333, ()=> console.log("Servidor rodando na porta 3333"));
+http.listen(3333, ()=> console.log("Porta rodando em http://localhost:3333/settings/admin"));
 
 
 /**
